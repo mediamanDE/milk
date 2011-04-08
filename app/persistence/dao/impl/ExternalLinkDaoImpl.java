bc043a4c49efa9459501063093587f2913821669
Michael Giere <michael.giere@mediaman-technology.de>
2011-04-08 16:38:06 +0200

    Added empty daos (interfaces and implementations)

---
+++ app/persistence/dao/impl/ExternalLinkDaoImpl.java
@@ -0,0 +1,8 @@
+package persistence.dao.impl;
+
+import persistence.dao.api.IExternalLinkDao;
+import persistence.dao.api.IMessageDao;
+
+public class ExternalLinkDaoImpl extends BaseDao implements IExternalLinkDao {
+
+}
