5f2f3579d7a3c9276eb22b77bf781987c75c3c7f
Frank Deberle <f.deberle@praemium-data.de>
2011-04-08 17:42:45 +0200

    New methods for interface of User

--- app/persistence/dao/api/IUserDao.java
+++ app/persistence/dao/api/IUserDao.java
@@ -1,5 +1,10 @@
 package persistence.dao.api;
 
+import models.User;
+
 public interface IUserDao {
+
+    public void store(User user); // Create and update
+    public User getByOpenId(String openId);
 
 }
