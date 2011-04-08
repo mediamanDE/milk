5d62bd5c5c50bc4a2cdb80ec70834cf04279d1a2
danielpehnec <pehnec@mediman.de>
2011-04-08 16:27:11 +0200

    Structure for GlobalTimeline

--- app/controllers/GlobalTimeline.java
+++ app/controllers/GlobalTimeline.java
@@ -1,15 +1,17 @@
 package controllers;
 
 import play.mvc.Controller;
+import java.util.*;
+
+import models.Message;
 
 public class GlobalTimeline extends Controller {
 	
 	public static void timeline(){
 		
+		List<Message> currentMessages = null;
 		
-		
-		
-		render();
+		render(currentMessages);
 		
 	}
 
