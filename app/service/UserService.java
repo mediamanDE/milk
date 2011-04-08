02b36b3bbbbf58c52477e6cfbed9d350b91c7391
Michael Giere <michael.giere@mediaman-technology.de>
2011-04-08 17:54:20 +0200

    Prepared Userservice for dao usage

--- app/service/UserService.java
+++ app/service/UserService.java
@@ -1,10 +1,20 @@
 package service;
 
 import models.User;
+import persistence.dao.api.IUserDao;
+import persistence.dao.impl.UserDaoImpl;
 
 public class UserService {
 	
-	public User getUserByOpenId(final String openId){
+	private IUserDao userDao;
+
+	public UserService() {
+		userDao = new UserDaoImpl();
+	}
+	
+	public User getUserByOpenId(final String openId) {
+		//User user = userDao.getUserByOpenId(openId);
+		
 		User user = new User();
 		user.setOpenId(openId);
 		//TODO 
@@ -13,6 +23,6 @@ public class UserService {
 	}
 	
 	public void storeUser(User user){
-		
+		//userDao.store(user);
 	}
 }
