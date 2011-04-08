42c78601d0ee4581b23e39e9f526edaabb6eee49
danielpehnec <pehnec@mediman.de>
2011-04-08 16:38:00 +0200

    Changes in GlobalTimeline

--- app/controllers/GlobalTimeline.java
+++ app/controllers/GlobalTimeline.java
@@ -1,10 +1,13 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.With;
+
 import java.util.*;
 
 import models.Message;
 
+@With(Authentication.class)
 public class GlobalTimeline extends Controller {
 	
 	public static void timeline(){
