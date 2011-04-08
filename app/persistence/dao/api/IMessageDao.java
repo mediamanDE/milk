a555682ea18c84fc708e1d240ab95a74cb87f49f
Carsten Sandtner <mail@casa-rock.de>
2011-04-08 18:35:17 +0200

    Added Interface declarations for Messages

--- app/persistence/dao/api/IMessageDao.java
+++ app/persistence/dao/api/IMessageDao.java
@@ -1,5 +1,46 @@
 package persistence.dao.api;
 
+import models.Group;
+import models.Message;
+
 public interface IMessageDao {
+
+    /**
+     * store Message
+     * @param message
+     */
+    public void store(Message message);
+
+    /**
+     * Delete Message
+     */
+    public void delete();
+
+    /**
+     * Get one Message by ID
+     * @param messageId
+     * @return Message
+     */
+    public Message getMessageById(String messageId);
+    
+    /**
+     * Get a thread
+     * @param messageId
+     * @return
+     */
+    public Message[] getThread(String messageId);
+
+    /**
+     * Get All Messages
+     * @return
+     */
+    public Message[] getAllMessages();
+
+    /**
+     * Get All Messages by Group
+     * @param group
+     * @return
+     */
+    public Message[] getAllMessagesByGroup(Group group);
 
 }
