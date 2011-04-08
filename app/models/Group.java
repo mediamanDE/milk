4b19dd7fd1c0d652e701d92faf0e57b97c95f0e6
Mitja Orzeszko <mitja.orzeszko@mediaman-technology.de>
2011-04-08 15:56:15 +0200

    generated getters and setters

--- app/models/Group.java
+++ app/models/Group.java
@@ -4,4 +4,41 @@ public class Group {
     private int _id;
     private String name;
     private String description;
+    
+	/**
+	 * @return the _id
+	 */
+	public int get_id() {
+		return _id;
+	}
+	/**
+	 * @param _id the _id to set
+	 */
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,5 +1,7 @@
 package models;
 
+import com.mongodb.Mongo;
+
 public class User {
 	private int _id;
 	private String openId;
@@ -10,4 +12,113 @@ public class User {
 	private int postCount;
 	private String timezone;
 	private ExternalLink[] externalLinks;
+	
+	/**
+	 * @return the _id
+	 */
+	public int get_id() {
+		return _id;
+	}
+	/**
+	 * @param _id the _id to set
+	 */
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+	/**
+	 * @return the openId
+	 */
+	public String getOpenId() {
+		return openId;
+	}
+	/**
+	 * @param openId the openId to set
+	 */
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+	/**
+	 * @return the nickname
+	 */
+	public String getNickname() {
+		return nickname;
+	}
+	/**
+	 * @param nickname the nickname to set
+	 */
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	/**
+	 * @return the fullname
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+	/**
+	 * @param fullname the fullname to set
+	 */
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	/**
+	 * @return the displayname
+	 */
+	public String getDisplayname() {
+		return displayname;
+	}
+	/**
+	 * @param displayname the displayname to set
+	 */
+	public void setDisplayname(String displayname) {
+		this.displayname = displayname;
+	}
+	/**
+	 * @return the avatarUrl
+	 */
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+	/**
+	 * @param avatarUrl the avatarUrl to set
+	 */
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
+	}
+	/**
+	 * @return the postCount
+	 */
+	public int getPostCount() {
+		return postCount;
+	}
+	/**
+	 * @param postCount the postCount to set
+	 */
+	public void setPostCount(int postCount) {
+		this.postCount = postCount;
+	}
+	/**
+	 * @return the timezone
+	 */
+	public String getTimezone() {
+		return timezone;
+	}
+	/**
+	 * @param timezone the timezone to set
+	 */
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+	/**
+	 * @return the externalLinks
+	 */
+	public ExternalLink[] getExternalLinks() {
+		return externalLinks;
+	}
+	/**
+	 * @param externalLinks the externalLinks to set
+	 */
+	public void setExternalLinks(ExternalLink[] externalLinks) {
+		this.externalLinks = externalLinks;
+	}
 }
