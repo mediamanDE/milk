75b059b1bd62edc2ec71abe0fe42e40b6e67a6a7
Mitja Orzeszko <mitja.orzeszko@mediaman-technology.de>
2011-04-09 09:36:16 +0200

    Auto-generated methods

--- app/persistence/dao/impl/GroupDaoImpl.java
+++ app/persistence/dao/impl/GroupDaoImpl.java
@@ -1,7 +1,26 @@
 package persistence.dao.impl;
 
+import models.Group;
 import persistence.dao.api.IGroup;
 
 public class GroupDaoImpl extends BaseDao implements IGroup {
+
+	@Override
+	public void store(Group group) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Group getGroupByName(String groupName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Group[] getGroups() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- app/persistence/dao/impl/MessageDaoImpl.java
+++ app/persistence/dao/impl/MessageDaoImpl.java
@@ -1,7 +1,45 @@
 package persistence.dao.impl;
 
+import models.Group;
+import models.Message;
 import persistence.dao.api.IMessageDao;
 
 public class MessageDaoImpl extends BaseDao implements IMessageDao {
+
+	@Override
+	public void store(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Message getMessageById(String messageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Message[] getThread(String messageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Message[] getAllMessages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Message[] getAllMessagesByGroup(Group group) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
