7cbb7e07b6c9f95ab53e7478c4b44048a66f1d5f
Benjamin Reitzammer <benjamin@ambestengestern.de>
2011-04-09 12:30:16 +0200

    finished deployment & build script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+DIST_DIR='milk-dist'
 PROJECT_NAME='milk-deploy'
-WAR_OUTPUT='milk-war'
+WAR_OUTPUT='milk'
 
+mkdir $DIST_DIR && cd $DIST_DIR
+
+# get and unpack the latest revision from github
 TAR_FILE=milk_`date +%Y%m%d-%H%M`.tar.gz
 wget -O $TAR_FILE --no-check-certificate https://github.com/mediaman-technology/milk/tarball/master
 tar xfz $TAR_FILE
+
+# identify and rename the unpacked directory to a easier-to-handle name
 TAR_DIR=`find .  -type d -name "mediaman-technology*" | sed s:\.\/::`
 mv $TAR_DIR $PROJECT_NAME
 
+# now actually build the war file
 play war $PROJECT_NAME -o $WAR_OUTPUT --zip
 
