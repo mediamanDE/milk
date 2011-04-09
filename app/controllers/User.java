e7cf4353ec0e5704aad3d7a5519369a27ddf46f4
danielpehnec <pehnec@mediman.de>
2011-04-09 13:27:43 +0200

    Profile User changed

--- app/controllers/User.java
+++ app/controllers/User.java
@@ -2,12 +2,19 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.With;
+import service.UserService;
+
 
 @With(Authentication.class)
 public class User extends Controller {
 	
 	public static void profile(String username) {
-		render(username);
+		
+		
+		models.User personalUser =  UserService.getUserByOpenId(session.get("user"));
+		
+		render(username, personalUser);
+	
 	}
 	
 	public static void edit(String username) {
