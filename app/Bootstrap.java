fe6d062753b88e260ad4b53c593748ca2fc2776d
Benjamin Reitzammer <benjamin@ambestengestern.de>
2011-04-09 14:01:47 +0200

    fixing routing in production mode

---
+++ app/Bootstrap.java
@@ -0,0 +1,17 @@
+import play.Play;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.mvc.Router;
+
+
+@OnApplicationStart
+public class Bootstrap extends Job {
+
+	public void doJob() {
+		String appMode = Play.configuration.getProperty("application.mode", "dev");
+		if ( "prod".equals(appMode) ) {
+			Play.ctxPath = Play.configuration.getProperty("context.path", "/milk");
+			Router.load(Play.ctxPath);
+		}
+	}
+}
